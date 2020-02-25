368f5a51419fe86ca327913a573382d082bf09c3
Benjamin Wilson <benjaminw1030@gmail.com>
2020-02-24 22:09:32 -0800

    add javascript to pull values from form

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,125 @@ $(document).ready(function() {
     var rust = 0;
     var swift = 0;
 
-  })
-})
+    var object = $("#object").val();
+    if (object === "yes") {
+      cpp += 1;
+      csharp += 1;
+      java += 1;
+      javascript += 1;
+      perl += 1;
+      php += 1;
+      python += 1;
+      ruby += 1;
+      rust +=1;
+      swift +=1;
+    }
+    else {
+      go += 1;
+    }
+
+    var functional = $("#functional").val();
+    if (functional === "yes") {
+      cpp += 1;
+      csharp += 1;
+      java += 1;
+      javascript += 1;
+      perl += 1;
+      php += 1;
+      python += 1;
+      ruby += 1;
+      rust +=1;
+      swift +=1;
+    }
+    else {
+      go += 1;
+    }    
+
+    var procedural = $("#procedural").val();
+    if (procedural === "yes") {
+      cpp += 1;
+      csharp += 1;
+      go += 1;
+      java += 1;
+      javascript += 1;
+      perl += 1;
+      php += 1;
+      python += 1;
+      rust +=1;
+      swift +=1;
+    }
+    else {
+      ruby += 1;
+    }
+
+    var generic = $("#generic").val();
+    if (generic === "yes") {
+      cpp += 1;
+      csharp += 1;
+      go += 1;
+      java += 1;
+      perl += 1;
+      python += 1;
+      rust +=1;
+      swift +=1;
+    }
+    else {
+      javascript += 1;
+      php += 1;
+      ruby +=1;
+    }
+    
+    var reflective = $("#reflective").val();
+    if (reflective === "yes") {
+      csharp += 1;
+      go += 1;
+      java += 1;
+      javascript += 1;
+      perl += 1;
+      php += 1;
+      python += 1;
+      ruby += 1;
+      swift +=1;
+    }
+    else {
+      cpp += 1;
+      rust += 1;
+    }
+
+    var event = $("#event").val();
+    if (event === "yes") {
+      csharp += 1;
+      go += 1;
+      java += 1;
+      javascript += 1;
+      python += 1;
+      rust +=1;
+      swift +=1;
+    }
+    else {
+      cpp += 1;
+      perl += 1;
+      php += 1;
+      ruby += 1;
+    }
+
+    var standardized = $("#standardized").val();
+    if (standardized === "yes") {
+      cpp += 1;
+      csharp += 1;
+      go += 1;
+      java += 1;
+      javascript += 1;
+      php += 1;
+      python += 1;
+      ruby += 1;
+    }
+    else {
+      perl += 1;
+      rust += 1;
+      swift += 1;
+    }
+    
+    
+  });
+});
