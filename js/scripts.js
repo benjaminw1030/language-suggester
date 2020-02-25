48c271e05f4820d4b0f03053d98cc2dea2c50e2c
Benjamin Wilson <benjaminw1030@gmail.com>
2020-02-24 21:47:57 -0800

    initial javascript, declare global variables

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("form#language").submit(function() {
+    event.preventDefault();
+    var cpp = 0;
+    var csharp = 0;
+    var go = 0;
+    var java = 0;
+    var javascript = 0;
+    var perl = 0;
+    var php = 0;
+    var python = 0;
+    var ruby = 0;
+    var rust = 0;
+    var swift = 0;
+
+  })
+})
